c2808ba850afa5941850f3f0d41e889c4832a239
kisslei1234 <359252078@qq.com>
2024-01-09 14:00:05 +0800

    testtest测试猜测是

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func quickSort(arr []int, l, r int) {
+	if l < r {
+		pivot := partition(arr, l, r)
+		quickSort(arr, l, pivot-1)
+		quickSort(arr, pivot+1, r)
+	}
+}
+
+func partition(arr []int, l, r int) int {
+	pivot := arr[l]
+	i := l + 1
+	j := r
+
+	for i <= j {
+		for i <= j && arr[i] <= pivot {
+			i++
+		}
+		for j >= i && arr[j] >= pivot {
+			j--
+		}
+		if i < j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	arr[l], arr[j] = arr[j], arr[l]
+	return j
+}
+func main() {
+	arr := []int{9, 5, 2, 7, 1, 8, 3}
+	quickSort(arr, 0, len(arr)-1)
+	fmt.Println(arr)
+}
